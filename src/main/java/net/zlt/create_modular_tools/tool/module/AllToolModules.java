68e3319e186a7f55563204d22f0aaee8994a2ef6
ZLT <manult002@gmail.com>
2024-11-10 14:00:51 -0300

    refactor unify tool module ids

--- src/main/java/net/zlt/create_modular_tools/tool/module/AllToolModules.java
+++ src/main/java/net/zlt/create_modular_tools/tool/module/AllToolModules.java
@@ -1,7 +1,7 @@
 package net.zlt.create_modular_tools.tool.module;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
-import net.zlt.create_modular_tools.CreateModularTools;
 import net.zlt.create_modular_tools.item.AllItems;
 import net.zlt.create_modular_tools.item.tool.module.ToolModuleItem;
 
@@ -9,98 +9,98 @@ public final class AllToolModules {
     private AllToolModules() {
     }
 
-    public static final ToolModuleItem WOODEN_SWORD_BLADE = register("sword_blade/wood", AllItems.WOODEN_SWORD_BLADE);
-    public static final ToolModuleItem STONE_SWORD_BLADE = register("sword_blade/stone", AllItems.STONE_SWORD_BLADE);
-    public static final ToolModuleItem IRON_SWORD_BLADE = register("sword_blade/iron", AllItems.IRON_SWORD_BLADE);
-    public static final ToolModuleItem COPPER_SWORD_BLADE = register("sword_blade/copper", AllItems.COPPER_SWORD_BLADE);
-    public static final ToolModuleItem GOLDEN_SWORD_BLADE = register("sword_blade/gold", AllItems.GOLDEN_SWORD_BLADE);
-    public static final ToolModuleItem NETHERITE_SWORD_BLADE = register("sword_blade/netherite", AllItems.NETHERITE_SWORD_BLADE);
-    public static final ToolModuleItem ZINC_SWORD_BLADE = register("sword_blade/zinc", AllItems.ZINC_SWORD_BLADE);
-    public static final ToolModuleItem BRASS_SWORD_BLADE = register("sword_blade/brass", AllItems.BRASS_SWORD_BLADE);
-    public static final ToolModuleItem DIAMOND_SWORD_BLADE = register("sword_blade/diamond", AllItems.DIAMOND_SWORD_BLADE);
+    public static final ToolModuleItem WOODEN_SWORD_BLADE = register(AllItems.WOODEN_SWORD_BLADE);
+    public static final ToolModuleItem STONE_SWORD_BLADE = register(AllItems.STONE_SWORD_BLADE);
+    public static final ToolModuleItem IRON_SWORD_BLADE = register(AllItems.IRON_SWORD_BLADE);
+    public static final ToolModuleItem COPPER_SWORD_BLADE = register(AllItems.COPPER_SWORD_BLADE);
+    public static final ToolModuleItem GOLDEN_SWORD_BLADE = register(AllItems.GOLDEN_SWORD_BLADE);
+    public static final ToolModuleItem NETHERITE_SWORD_BLADE = register(AllItems.NETHERITE_SWORD_BLADE);
+    public static final ToolModuleItem ZINC_SWORD_BLADE = register(AllItems.ZINC_SWORD_BLADE);
+    public static final ToolModuleItem BRASS_SWORD_BLADE = register(AllItems.BRASS_SWORD_BLADE);
+    public static final ToolModuleItem DIAMOND_SWORD_BLADE = register(AllItems.DIAMOND_SWORD_BLADE);
 
-    public static final ToolModuleItem WOODEN_SHOVEL_HEAD = register("shovel_head/wood", AllItems.WOODEN_SHOVEL_HEAD);
-    public static final ToolModuleItem STONE_SHOVEL_HEAD = register("shovel_head/stone", AllItems.STONE_SHOVEL_HEAD);
-    public static final ToolModuleItem IRON_SHOVEL_HEAD = register("shovel_head/iron", AllItems.IRON_SHOVEL_HEAD);
-    public static final ToolModuleItem COPPER_SHOVEL_HEAD = register("shovel_head/copper", AllItems.COPPER_SHOVEL_HEAD);
-    public static final ToolModuleItem GOLDEN_SHOVEL_HEAD = register("shovel_head/gold", AllItems.GOLDEN_SHOVEL_HEAD);
-    public static final ToolModuleItem NETHERITE_SHOVEL_HEAD = register("shovel_head/netherite", AllItems.NETHERITE_SHOVEL_HEAD);
-    public static final ToolModuleItem ZINC_SHOVEL_HEAD = register("shovel_head/zinc", AllItems.ZINC_SHOVEL_HEAD);
-    public static final ToolModuleItem BRASS_SHOVEL_HEAD = register("shovel_head/brass", AllItems.BRASS_SHOVEL_HEAD);
-    public static final ToolModuleItem DIAMOND_SHOVEL_HEAD = register("shovel_head/diamond", AllItems.DIAMOND_SHOVEL_HEAD);
+    public static final ToolModuleItem WOODEN_SHOVEL_HEAD = register(AllItems.WOODEN_SHOVEL_HEAD);
+    public static final ToolModuleItem STONE_SHOVEL_HEAD = register(AllItems.STONE_SHOVEL_HEAD);
+    public static final ToolModuleItem IRON_SHOVEL_HEAD = register(AllItems.IRON_SHOVEL_HEAD);
+    public static final ToolModuleItem COPPER_SHOVEL_HEAD = register(AllItems.COPPER_SHOVEL_HEAD);
+    public static final ToolModuleItem GOLDEN_SHOVEL_HEAD = register(AllItems.GOLDEN_SHOVEL_HEAD);
+    public static final ToolModuleItem NETHERITE_SHOVEL_HEAD = register(AllItems.NETHERITE_SHOVEL_HEAD);
+    public static final ToolModuleItem ZINC_SHOVEL_HEAD = register(AllItems.ZINC_SHOVEL_HEAD);
+    public static final ToolModuleItem BRASS_SHOVEL_HEAD = register(AllItems.BRASS_SHOVEL_HEAD);
+    public static final ToolModuleItem DIAMOND_SHOVEL_HEAD = register(AllItems.DIAMOND_SHOVEL_HEAD);
 
-    public static final ToolModuleItem WOODEN_PICKAXE_HEAD = register("pickaxe_head/wood", AllItems.WOODEN_PICKAXE_HEAD);
-    public static final ToolModuleItem STONE_PICKAXE_HEAD = register("pickaxe_head/stone", AllItems.STONE_PICKAXE_HEAD);
-    public static final ToolModuleItem IRON_PICKAXE_HEAD = register("pickaxe_head/iron", AllItems.IRON_PICKAXE_HEAD);
-    public static final ToolModuleItem COPPER_PICKAXE_HEAD = register("pickaxe_head/copper", AllItems.COPPER_PICKAXE_HEAD);
-    public static final ToolModuleItem GOLDEN_PICKAXE_HEAD = register("pickaxe_head/gold", AllItems.GOLDEN_PICKAXE_HEAD);
-    public static final ToolModuleItem NETHERITE_PICKAXE_HEAD = register("pickaxe_head/netherite", AllItems.NETHERITE_PICKAXE_HEAD);
-    public static final ToolModuleItem ZINC_PICKAXE_HEAD = register("pickaxe_head/zinc", AllItems.ZINC_PICKAXE_HEAD);
-    public static final ToolModuleItem BRASS_PICKAXE_HEAD = register("pickaxe_head/brass", AllItems.BRASS_PICKAXE_HEAD);
-    public static final ToolModuleItem DIAMOND_PICKAXE_HEAD = register("pickaxe_head/diamond", AllItems.DIAMOND_PICKAXE_HEAD);
+    public static final ToolModuleItem WOODEN_PICKAXE_HEAD = register(AllItems.WOODEN_PICKAXE_HEAD);
+    public static final ToolModuleItem STONE_PICKAXE_HEAD = register(AllItems.STONE_PICKAXE_HEAD);
+    public static final ToolModuleItem IRON_PICKAXE_HEAD = register(AllItems.IRON_PICKAXE_HEAD);
+    public static final ToolModuleItem COPPER_PICKAXE_HEAD = register(AllItems.COPPER_PICKAXE_HEAD);
+    public static final ToolModuleItem GOLDEN_PICKAXE_HEAD = register(AllItems.GOLDEN_PICKAXE_HEAD);
+    public static final ToolModuleItem NETHERITE_PICKAXE_HEAD = register(AllItems.NETHERITE_PICKAXE_HEAD);
+    public static final ToolModuleItem ZINC_PICKAXE_HEAD = register(AllItems.ZINC_PICKAXE_HEAD);
+    public static final ToolModuleItem BRASS_PICKAXE_HEAD = register(AllItems.BRASS_PICKAXE_HEAD);
+    public static final ToolModuleItem DIAMOND_PICKAXE_HEAD = register(AllItems.DIAMOND_PICKAXE_HEAD);
 
-    public static final ToolModuleItem WOODEN_AXE_HEAD = register("axe_head/wood", AllItems.WOODEN_AXE_HEAD);
-    public static final ToolModuleItem STONE_AXE_HEAD = register("axe_head/stone", AllItems.STONE_AXE_HEAD);
-    public static final ToolModuleItem IRON_AXE_HEAD = register("axe_head/iron", AllItems.IRON_AXE_HEAD);
-    public static final ToolModuleItem COPPER_AXE_HEAD = register("axe_head/copper", AllItems.COPPER_AXE_HEAD);
-    public static final ToolModuleItem GOLDEN_AXE_HEAD = register("axe_head/gold", AllItems.GOLDEN_AXE_HEAD);
-    public static final ToolModuleItem NETHERITE_AXE_HEAD = register("axe_head/netherite", AllItems.NETHERITE_AXE_HEAD);
-    public static final ToolModuleItem ZINC_AXE_HEAD = register("axe_head/zinc", AllItems.ZINC_AXE_HEAD);
-    public static final ToolModuleItem BRASS_AXE_HEAD = register("axe_head/brass", AllItems.BRASS_AXE_HEAD);
-    public static final ToolModuleItem DIAMOND_AXE_HEAD = register("axe_head/diamond", AllItems.DIAMOND_AXE_HEAD);
+    public static final ToolModuleItem WOODEN_AXE_HEAD = register(AllItems.WOODEN_AXE_HEAD);
+    public static final ToolModuleItem STONE_AXE_HEAD = register(AllItems.STONE_AXE_HEAD);
+    public static final ToolModuleItem IRON_AXE_HEAD = register(AllItems.IRON_AXE_HEAD);
+    public static final ToolModuleItem COPPER_AXE_HEAD = register(AllItems.COPPER_AXE_HEAD);
+    public static final ToolModuleItem GOLDEN_AXE_HEAD = register(AllItems.GOLDEN_AXE_HEAD);
+    public static final ToolModuleItem NETHERITE_AXE_HEAD = register(AllItems.NETHERITE_AXE_HEAD);
+    public static final ToolModuleItem ZINC_AXE_HEAD = register(AllItems.ZINC_AXE_HEAD);
+    public static final ToolModuleItem BRASS_AXE_HEAD = register(AllItems.BRASS_AXE_HEAD);
+    public static final ToolModuleItem DIAMOND_AXE_HEAD = register(AllItems.DIAMOND_AXE_HEAD);
 
-    public static final ToolModuleItem WOODEN_HOE_HEAD = register("hoe_head/wood", AllItems.WOODEN_HOE_HEAD);
-    public static final ToolModuleItem STONE_HOE_HEAD = register("hoe_head/stone", AllItems.STONE_HOE_HEAD);
-    public static final ToolModuleItem IRON_HOE_HEAD = register("hoe_head/iron", AllItems.IRON_HOE_HEAD);
-    public static final ToolModuleItem COPPER_HOE_HEAD = register("hoe_head/copper", AllItems.COPPER_HOE_HEAD);
-    public static final ToolModuleItem GOLDEN_HOE_HEAD = register("hoe_head/gold", AllItems.GOLDEN_HOE_HEAD);
-    public static final ToolModuleItem NETHERITE_HOE_HEAD = register("hoe_head/netherite", AllItems.NETHERITE_HOE_HEAD);
-    public static final ToolModuleItem ZINC_HOE_HEAD = register("hoe_head/zinc", AllItems.ZINC_HOE_HEAD);
-    public static final ToolModuleItem BRASS_HOE_HEAD = register("hoe_head/brass", AllItems.BRASS_HOE_HEAD);
-    public static final ToolModuleItem DIAMOND_HOE_HEAD = register("hoe_head/diamond", AllItems.DIAMOND_HOE_HEAD);
+    public static final ToolModuleItem WOODEN_HOE_HEAD = register(AllItems.WOODEN_HOE_HEAD);
+    public static final ToolModuleItem STONE_HOE_HEAD = register(AllItems.STONE_HOE_HEAD);
+    public static final ToolModuleItem IRON_HOE_HEAD = register(AllItems.IRON_HOE_HEAD);
+    public static final ToolModuleItem COPPER_HOE_HEAD = register(AllItems.COPPER_HOE_HEAD);
+    public static final ToolModuleItem GOLDEN_HOE_HEAD = register(AllItems.GOLDEN_HOE_HEAD);
+    public static final ToolModuleItem NETHERITE_HOE_HEAD = register(AllItems.NETHERITE_HOE_HEAD);
+    public static final ToolModuleItem ZINC_HOE_HEAD = register(AllItems.ZINC_HOE_HEAD);
+    public static final ToolModuleItem BRASS_HOE_HEAD = register(AllItems.BRASS_HOE_HEAD);
+    public static final ToolModuleItem DIAMOND_HOE_HEAD = register(AllItems.DIAMOND_HOE_HEAD);
 
-    public static final ToolModuleItem WOODEN_TOOL_HANDLE = register("tool_handle/wood", AllItems.WOODEN_TOOL_HANDLE);
-    public static final ToolModuleItem STONE_TOOL_HANDLE = register("tool_handle/stone", AllItems.STONE_TOOL_HANDLE);
-    public static final ToolModuleItem IRON_TOOL_HANDLE = register("tool_handle/iron", AllItems.IRON_TOOL_HANDLE);
-    public static final ToolModuleItem COPPER_TOOL_HANDLE = register("tool_handle/copper", AllItems.COPPER_TOOL_HANDLE);
-    public static final ToolModuleItem GOLDEN_TOOL_HANDLE = register("tool_handle/gold", AllItems.GOLDEN_TOOL_HANDLE);
-    public static final ToolModuleItem NETHERITE_TOOL_HANDLE = register("tool_handle/netherite", AllItems.NETHERITE_TOOL_HANDLE);
-    public static final ToolModuleItem ZINC_TOOL_HANDLE = register("tool_handle/zinc", AllItems.ZINC_TOOL_HANDLE);
-    public static final ToolModuleItem BRASS_TOOL_HANDLE = register("tool_handle/brass", AllItems.BRASS_TOOL_HANDLE);
-    public static final ToolModuleItem DIAMOND_TOOL_HANDLE = register("tool_handle/diamond", AllItems.DIAMOND_TOOL_HANDLE);
+    public static final ToolModuleItem WOODEN_TOOL_HANDLE = register(AllItems.WOODEN_TOOL_HANDLE);
+    public static final ToolModuleItem STONE_TOOL_HANDLE = register(AllItems.STONE_TOOL_HANDLE);
+    public static final ToolModuleItem IRON_TOOL_HANDLE = register(AllItems.IRON_TOOL_HANDLE);
+    public static final ToolModuleItem COPPER_TOOL_HANDLE = register(AllItems.COPPER_TOOL_HANDLE);
+    public static final ToolModuleItem GOLDEN_TOOL_HANDLE = register(AllItems.GOLDEN_TOOL_HANDLE);
+    public static final ToolModuleItem NETHERITE_TOOL_HANDLE = register(AllItems.NETHERITE_TOOL_HANDLE);
+    public static final ToolModuleItem ZINC_TOOL_HANDLE = register(AllItems.ZINC_TOOL_HANDLE);
+    public static final ToolModuleItem BRASS_TOOL_HANDLE = register(AllItems.BRASS_TOOL_HANDLE);
+    public static final ToolModuleItem DIAMOND_TOOL_HANDLE = register(AllItems.DIAMOND_TOOL_HANDLE);
 
-    public static final ToolModuleItem WOODEN_TOOL_GRIP = register("tool_grip/wood", AllItems.WOODEN_TOOL_GRIP);
-    public static final ToolModuleItem STONE_TOOL_GRIP = register("tool_grip/stone", AllItems.STONE_TOOL_GRIP);
-    public static final ToolModuleItem IRON_TOOL_GRIP = register("tool_grip/iron", AllItems.IRON_TOOL_GRIP);
-    public static final ToolModuleItem COPPER_TOOL_GRIP = register("tool_grip/copper", AllItems.COPPER_TOOL_GRIP);
-    public static final ToolModuleItem GOLDEN_TOOL_GRIP = register("tool_grip/gold", AllItems.GOLDEN_TOOL_GRIP);
-    public static final ToolModuleItem NETHERITE_TOOL_GRIP = register("tool_grip/netherite", AllItems.NETHERITE_TOOL_GRIP);
-    public static final ToolModuleItem ZINC_TOOL_GRIP = register("tool_grip/zinc", AllItems.ZINC_TOOL_GRIP);
-    public static final ToolModuleItem BRASS_TOOL_GRIP = register("tool_grip/brass", AllItems.BRASS_TOOL_GRIP);
-    public static final ToolModuleItem DIAMOND_TOOL_GRIP = register("tool_grip/diamond", AllItems.DIAMOND_TOOL_GRIP);
+    public static final ToolModuleItem WOODEN_TOOL_GRIP = register(AllItems.WOODEN_TOOL_GRIP);
+    public static final ToolModuleItem STONE_TOOL_GRIP = register(AllItems.STONE_TOOL_GRIP);
+    public static final ToolModuleItem IRON_TOOL_GRIP = register(AllItems.IRON_TOOL_GRIP);
+    public static final ToolModuleItem COPPER_TOOL_GRIP = register(AllItems.COPPER_TOOL_GRIP);
+    public static final ToolModuleItem GOLDEN_TOOL_GRIP = register(AllItems.GOLDEN_TOOL_GRIP);
+    public static final ToolModuleItem NETHERITE_TOOL_GRIP = register(AllItems.NETHERITE_TOOL_GRIP);
+    public static final ToolModuleItem ZINC_TOOL_GRIP = register(AllItems.ZINC_TOOL_GRIP);
+    public static final ToolModuleItem BRASS_TOOL_GRIP = register(AllItems.BRASS_TOOL_GRIP);
+    public static final ToolModuleItem DIAMOND_TOOL_GRIP = register(AllItems.DIAMOND_TOOL_GRIP);
 
-    public static final ToolModuleItem WOODEN_SWORD_GUARD = register("sword_guard/wood", AllItems.WOODEN_SWORD_GUARD);
-    public static final ToolModuleItem STONE_SWORD_GUARD = register("sword_guard/stone", AllItems.STONE_SWORD_GUARD);
-    public static final ToolModuleItem IRON_SWORD_GUARD = register("sword_guard/iron", AllItems.IRON_SWORD_GUARD);
-    public static final ToolModuleItem COPPER_SWORD_GUARD = register("sword_guard/copper", AllItems.COPPER_SWORD_GUARD);
-    public static final ToolModuleItem GOLDEN_SWORD_GUARD = register("sword_guard/gold", AllItems.GOLDEN_SWORD_GUARD);
-    public static final ToolModuleItem NETHERITE_SWORD_GUARD = register("sword_guard/netherite", AllItems.NETHERITE_SWORD_GUARD);
-    public static final ToolModuleItem ZINC_SWORD_GUARD = register("sword_guard/zinc", AllItems.ZINC_SWORD_GUARD);
-    public static final ToolModuleItem BRASS_SWORD_GUARD = register("sword_guard/brass", AllItems.BRASS_SWORD_GUARD);
-    public static final ToolModuleItem DIAMOND_SWORD_GUARD = register("sword_guard/diamond", AllItems.DIAMOND_SWORD_GUARD);
+    public static final ToolModuleItem WOODEN_SWORD_GUARD = register(AllItems.WOODEN_SWORD_GUARD);
+    public static final ToolModuleItem STONE_SWORD_GUARD = register(AllItems.STONE_SWORD_GUARD);
+    public static final ToolModuleItem IRON_SWORD_GUARD = register(AllItems.IRON_SWORD_GUARD);
+    public static final ToolModuleItem COPPER_SWORD_GUARD = register(AllItems.COPPER_SWORD_GUARD);
+    public static final ToolModuleItem GOLDEN_SWORD_GUARD = register(AllItems.GOLDEN_SWORD_GUARD);
+    public static final ToolModuleItem NETHERITE_SWORD_GUARD = register(AllItems.NETHERITE_SWORD_GUARD);
+    public static final ToolModuleItem ZINC_SWORD_GUARD = register(AllItems.ZINC_SWORD_GUARD);
+    public static final ToolModuleItem BRASS_SWORD_GUARD = register(AllItems.BRASS_SWORD_GUARD);
+    public static final ToolModuleItem DIAMOND_SWORD_GUARD = register(AllItems.DIAMOND_SWORD_GUARD);
 
-    public static final ToolModuleItem WOODEN_SWORD_POMMEL = register("sword_pommel/wood", AllItems.WOODEN_SWORD_POMMEL);
-    public static final ToolModuleItem STONE_SWORD_POMMEL = register("sword_pommel/stone", AllItems.STONE_SWORD_POMMEL);
-    public static final ToolModuleItem IRON_SWORD_POMMEL = register("sword_pommel/iron", AllItems.IRON_SWORD_POMMEL);
-    public static final ToolModuleItem COPPER_SWORD_POMMEL = register("sword_pommel/copper", AllItems.COPPER_SWORD_POMMEL);
-    public static final ToolModuleItem GOLDEN_SWORD_POMMEL = register("sword_pommel/gold", AllItems.GOLDEN_SWORD_POMMEL);
-    public static final ToolModuleItem NETHERITE_SWORD_POMMEL = register("sword_pommel/netherite", AllItems.NETHERITE_SWORD_POMMEL);
-    public static final ToolModuleItem ZINC_SWORD_POMMEL = register("sword_pommel/zinc", AllItems.ZINC_SWORD_POMMEL);
-    public static final ToolModuleItem BRASS_SWORD_POMMEL = register("sword_pommel/brass", AllItems.BRASS_SWORD_POMMEL);
-    public static final ToolModuleItem DIAMOND_SWORD_POMMEL = register("sword_pommel/diamond", AllItems.DIAMOND_SWORD_POMMEL);
+    public static final ToolModuleItem WOODEN_SWORD_POMMEL = register(AllItems.WOODEN_SWORD_POMMEL);
+    public static final ToolModuleItem STONE_SWORD_POMMEL = register(AllItems.STONE_SWORD_POMMEL);
+    public static final ToolModuleItem IRON_SWORD_POMMEL = register(AllItems.IRON_SWORD_POMMEL);
+    public static final ToolModuleItem COPPER_SWORD_POMMEL = register(AllItems.COPPER_SWORD_POMMEL);
+    public static final ToolModuleItem GOLDEN_SWORD_POMMEL = register(AllItems.GOLDEN_SWORD_POMMEL);
+    public static final ToolModuleItem NETHERITE_SWORD_POMMEL = register(AllItems.NETHERITE_SWORD_POMMEL);
+    public static final ToolModuleItem ZINC_SWORD_POMMEL = register(AllItems.ZINC_SWORD_POMMEL);
+    public static final ToolModuleItem BRASS_SWORD_POMMEL = register(AllItems.BRASS_SWORD_POMMEL);
+    public static final ToolModuleItem DIAMOND_SWORD_POMMEL = register(AllItems.DIAMOND_SWORD_POMMEL);
 
-    private static ToolModuleItem register(String path, Item toolModule) {
-        return ToolModuleRegistry.register(CreateModularTools.asResource(path), toolModule);
+    private static ToolModuleItem register(Item toolModule) {
+        return ToolModuleRegistry.register(BuiltInRegistries.ITEM.getKey(toolModule), toolModule);
     }
     
     public static void init() {
