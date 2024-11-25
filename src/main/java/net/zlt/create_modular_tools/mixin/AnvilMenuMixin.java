803bf9b7bf30ea720a85792e73129405ec02aee5
ZLT <manult002@gmail.com>
2024-11-25 17:34:00 -0300

    refactor rename anvil menu mixin method

--- src/main/java/net/zlt/create_modular_tools/mixin/AnvilMenuMixin.java
+++ src/main/java/net/zlt/create_modular_tools/mixin/AnvilMenuMixin.java
@@ -20,7 +20,7 @@ public abstract class AnvilMenuMixin extends ItemCombinerMenu {
     }
 
     @ModifyExpressionValue(method = "createResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 0))
-    private boolean createModularTools$createResult(boolean original, @Local(ordinal = 0) ItemStack itemStack) {
+    private boolean createModularTools$ignoreSecondModularTool(boolean original, @Local(ordinal = 0) ItemStack itemStack) {
         return original || inputSlots.getItem(1).getItem() instanceof ModularToolItem;
     }
 }
