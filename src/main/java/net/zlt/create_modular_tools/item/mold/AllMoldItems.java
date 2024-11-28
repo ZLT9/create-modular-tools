d17e44354916162ccc32dd068b5ea6bf9954abdc
ZLT <manult002@gmail.com>
2024-11-28 14:27:02 -0300

    refactor use base mold item class

--- src/main/java/net/zlt/create_modular_tools/item/mold/AllMoldItems.java
+++ src/main/java/net/zlt/create_modular_tools/item/mold/AllMoldItems.java
@@ -14,11 +14,11 @@ public final class AllMoldItems {
     public static final Item SAND_MOLD = register("sand_mold", new BlockItem(AllMoldBlocks.SAND_MOLD, new Item.Properties()));
     public static final Item RED_SAND_MOLD = register("red_sand_mold", new BlockItem(AllMoldBlocks.RED_SAND_MOLD, new Item.Properties()));
 
-    public static final Item SWORD_SAND_MOLD = register("sword_sand_mold", new SwordSandMoldItem(AllMoldBlocks.SWORD_SAND_MOLD, new Item.Properties().stacksTo(1)));
-    public static final Item SHOVEL_SAND_MOLD = register("shovel_sand_mold", new ShovelSandMoldItem(AllMoldBlocks.SHOVEL_SAND_MOLD, new Item.Properties().stacksTo(1)));
-    public static final Item PICKAXE_SAND_MOLD = register("pickaxe_sand_mold", new PickaxeSandMoldItem(AllMoldBlocks.PICKAXE_SAND_MOLD, new Item.Properties().stacksTo(1)));
-    public static final Item AXE_SAND_MOLD = register("axe_sand_mold", new AxeSandMoldItem(AllMoldBlocks.AXE_SAND_MOLD, new Item.Properties().stacksTo(1)));
-    public static final Item HOE_SAND_MOLD = register("hoe_sand_mold", new HoeSandMoldItem(AllMoldBlocks.HOE_SAND_MOLD, new Item.Properties().stacksTo(1)));
+    public static final Item SWORD_SAND_MOLD = register("sword_sand_mold", new BaseSandMoldItem(AllMoldBlocks.SWORD_SAND_MOLD, new Item.Properties().stacksTo(1)));
+    public static final Item SHOVEL_SAND_MOLD = register("shovel_sand_mold", new BaseSandMoldItem(AllMoldBlocks.SHOVEL_SAND_MOLD, new Item.Properties().stacksTo(1)));
+    public static final Item PICKAXE_SAND_MOLD = register("pickaxe_sand_mold", new BaseSandMoldItem(AllMoldBlocks.PICKAXE_SAND_MOLD, new Item.Properties().stacksTo(1)));
+    public static final Item AXE_SAND_MOLD = register("axe_sand_mold", new BaseSandMoldItem(AllMoldBlocks.AXE_SAND_MOLD, new Item.Properties().stacksTo(1)));
+    public static final Item HOE_SAND_MOLD = register("hoe_sand_mold", new BaseSandMoldItem(AllMoldBlocks.HOE_SAND_MOLD, new Item.Properties().stacksTo(1)));
 
     private static Item register(String path, Item item) {
         return Registry.register(BuiltInRegistries.ITEM, CreateModularTools.asResource(path), item);
