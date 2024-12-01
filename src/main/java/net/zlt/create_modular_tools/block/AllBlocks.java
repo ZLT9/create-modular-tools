1c9a01894a1f290a8b310ed419da8e60cde355b2
ZLT <manult002@gmail.com>
2024-12-01 17:48:26 -0300

    chore use empty mold block class

--- src/main/java/net/zlt/create_modular_tools/block/AllBlocks.java
+++ src/main/java/net/zlt/create_modular_tools/block/AllBlocks.java
@@ -18,17 +18,17 @@ public final class AllBlocks {
     private AllBlocks() {
     }
 
-    public static final Block OAK_EMPTY_MOLD = register("oak_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS)));
-    public static final Block SPRUCE_EMPTY_MOLD = register("spruce_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.SPRUCE_PLANKS)));
-    public static final Block BIRCH_EMPTY_MOLD = register("birch_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.BIRCH_PLANKS)));
-    public static final Block JUNGLE_EMPTY_MOLD = register("jungle_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.JUNGLE_PLANKS)));
-    public static final Block ACACIA_EMPTY_MOLD = register("acacia_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.ACACIA_PLANKS)));
-    public static final Block DARK_OAK_EMPTY_MOLD = register("dark_oak_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.DARK_OAK_PLANKS)));
-    public static final Block MANGROVE_EMPTY_MOLD = register("mangrove_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.MANGROVE_PLANKS)));
-    public static final Block CHERRY_EMPTY_MOLD = register("cherry_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.CHERRY_PLANKS)));
-    public static final Block BAMBOO_EMPTY_MOLD = register("bamboo_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.BAMBOO_PLANKS)));
-    public static final Block CRIMSON_EMPTY_MOLD = register("crimson_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.CRIMSON_PLANKS)));
-    public static final Block WARPED_EMPTY_MOLD = register("warped_empty_mold", new Block(FabricBlockSettings.copyOf(Blocks.WARPED_PLANKS)));
+    public static final Block OAK_EMPTY_MOLD = register("oak_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS)));
+    public static final Block SPRUCE_EMPTY_MOLD = register("spruce_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.SPRUCE_PLANKS)));
+    public static final Block BIRCH_EMPTY_MOLD = register("birch_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.BIRCH_PLANKS)));
+    public static final Block JUNGLE_EMPTY_MOLD = register("jungle_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.JUNGLE_PLANKS)));
+    public static final Block ACACIA_EMPTY_MOLD = register("acacia_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_PLANKS)));
+    public static final Block DARK_OAK_EMPTY_MOLD = register("dark_oak_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_PLANKS)));
+    public static final Block MANGROVE_EMPTY_MOLD = register("mangrove_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.MANGROVE_PLANKS)));
+    public static final Block CHERRY_EMPTY_MOLD = register("cherry_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.CHERRY_PLANKS)));
+    public static final Block BAMBOO_EMPTY_MOLD = register("bamboo_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.BAMBOO_PLANKS)));
+    public static final Block CRIMSON_EMPTY_MOLD = register("crimson_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_PLANKS)));
+    public static final Block WARPED_EMPTY_MOLD = register("warped_empty_mold", new EmptyMoldBlock(FabricBlockSettings.copyOf(Blocks.WARPED_PLANKS)));
 
     public static final Block MOLTEN_IRON = register("molten_iron", new LiquidBlock(AllFluids.MOLTEN_IRON, FabricBlockSettings.copy(Blocks.LAVA)));
     public static final Block MOLTEN_COPPER = register("molten_copper", new LiquidBlock(AllFluids.MOLTEN_COPPER, FabricBlockSettings.copy(Blocks.LAVA)));
