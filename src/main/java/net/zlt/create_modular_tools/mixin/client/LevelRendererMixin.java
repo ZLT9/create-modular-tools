eabe07a9c5c38aca295b886c559b00826c8d56f3
ZLT <manult002@gmail.com>
2024-12-13 17:56:31 -0300

    refactor remove unnecessary condition

--- src/main/java/net/zlt/create_modular_tools/mixin/client/LevelRendererMixin.java
+++ src/main/java/net/zlt/create_modular_tools/mixin/client/LevelRendererMixin.java
@@ -8,12 +8,10 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.zlt.create_modular_tools.block.MoldBlock;
-import net.zlt.create_modular_tools.block.mold.MaterialMoldBlock;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,7 +26,6 @@ public abstract class LevelRendererMixin {
 
     @ModifyExpressionValue(method = "renderHitOutline", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
     private VoxelShape createModularTools$renderHitOutline(VoxelShape original, @Local(argsOnly = true) Entity entity, @Local(argsOnly = true) BlockPos pos, @Local(argsOnly = true) BlockState state) {
-        Block block = state.getBlock();
-        return block instanceof MaterialMoldBlock || block instanceof MoldBlock ? state.getVisualShape(level, pos, CollisionContext.of(entity)) : original;
+        return state.getBlock() instanceof MoldBlock ? state.getVisualShape(level, pos, CollisionContext.of(entity)) : original;
     }
 }
